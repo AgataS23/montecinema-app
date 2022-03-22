c68237a31ff221a083b1b8602406fad32597fdad
Agata Stoch <agata.stoch@monterail.com>
2022-03-22 19:51:23 +0100

    Added some icons in assets and created MainButton and TopMenu

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="navigation-bar">
+    <router-link
+      class="navigation-bar__item"
+      exact-active-class="top-menu-item--active"
+      :to="{ name: 'Movies' }"
+    >
+      Movies
+    </router-link>
+
+    <router-link
+      class="navigation-bar__item"
+      active-class="top-menu-item--active"
+      :to="{ name: 'Screenings' }"
+    >
+      Screenings
+    </router-link>
+
+    <router-link
+      class="navigation-bar__item"
+      active-class="top-menu-item--active"
+      :to="{ name: 'Contact' }"
+    >
+      Contact us
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavigationBar",
+};
+</script>
+<style lang="scss" scoped>
+.navigation-bar {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: baseline;
+  &__item {
+    padding: 4.55rem 2.7rem;
+    align-self: center;
+    text-decoration: none;
+    font: normal 400 1.8rem/2.1rem "Roboto";
+    color: $jumbo;
+  }
+  &__item:hover {
+    cursor: pointer;
+    color: $abbey;
+    padding-bottom: 4.25rem;
+    border-bottom: 3px solid $cherry-red;
+  }
+}
+</style>
---
+++ src/components/NavigationButtons.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="top-menu__buttons">
+    <router-link
+      class="top-menu-item"
+      active-class="top-menu-item--active"
+      :to="{ name: 'Register' }"
+    >
+      <ButtonMain medium empty borderNone color="primary">Register</ButtonMain>
+    </router-link>
+
+    <router-link
+      class="top-menu-item"
+      active-class="top-menu-item--active"
+      :to="{ name: 'Login' }"
+    >
+      <ButtonMain medium color="primary">Login</ButtonMain>
+    </router-link>
+  </div>
+</template>
+<script>
+import ButtonMain from "./ButtonMain.vue";
+export default {
+  name: "NavigationButtons",
+  components: {
+    ButtonMain,
+  },
+};
+</script>
+<style scoped>
+.top-menu__buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: baseline;
+}
+.top-menu-item {
+  text-decoration: none;
+}
+.top-menu-item:first-of-type {
+  margin-right: 1.6rem;
+}
+</style>
